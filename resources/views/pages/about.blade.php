dcdd2a0ba05460534ed107b9cab073ebba3ea3ba
Raphael Kalinowski <raphael@myminicom.com>
2022-04-29 11:17:06 +0200

    added section and layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,19 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout')
 
-        <title>Home</title>
-
-    </head>
-    <body>
+@section('content')
       <h1>About us</h1>
       <p>Page built with &hearts;</p>
       <p>back <a href="/">home</a></p>
-
-    <footer>
-        <p>&copy; Copyright {{ date('Y') }} &middot;</p>
-    </footer>
-    </body>
-</html>
+@endsection
