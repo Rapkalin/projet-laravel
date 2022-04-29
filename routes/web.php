59b745d1418226c79d6d9f61dec89e991919331d
Raphael Kalinowski <raphael@myminicom.com>
2022-04-29 11:11:06 +0200

    moved static pages to folder pages

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('pages.home');
 });
 
 Route::get('about-us', function () {
-   return view('about');
+   return view('pages.about');
 });
