53652d233ab8016a26f58fb5936bc5e32435b0c5
Raphael Kalinowski <raphael@myminicom.com>
2022-05-03 10:28:13 +0200

    update view with data

--- resources/views/pages/users.blade.php
+++ resources/views/pages/users.blade.php
@@ -1,4 +1,12 @@
 <h1>Here is the users page</h1>
-{{ $users }}
+@foreach($users as $user)
 
+    <div>
+        <li> User n°{{ $user->id }}</li>
+        <li> Name {{ $user->name }}</li>
+        <li> Email {{ $user->email }}</li>
+        <br>
+    </div>
+
+ @endforeach
 <p>fin</p>
