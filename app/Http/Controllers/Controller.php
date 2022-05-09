53880ac62bae7b613dcb307e92a9dd04a1bd23e7
Raphael Kalinowski <raphael@myminicom.com>
2022-05-09 12:11:48 +0200

    make Controller abstract creat AppController

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,17 +7,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function index ()
-    {
-        return view('pages/home');
-    }
-
-    public function about ()
-    {
-        return view('pages/about');
-    }
 }
