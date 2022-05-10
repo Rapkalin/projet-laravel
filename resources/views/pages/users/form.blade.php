100966d88f69fcfaebf9bc657d98b7ee04656674
Raphael Kalinowski <raphael@myminicom.com>
2022-05-10 18:19:19 +0200

    added links from users index to user edit

--- resources/views/pages/users/form.blade.php
+++ resources/views/pages/users/form.blade.php
@@ -4,7 +4,7 @@
 <h1>Here is the edit page for the user</h1>
 
 <div>
-    {!! Form::model($user, ['route' => ['userUpdate', $user->id]]) !!}
+    {!! Form::model($user, ['route' => ['userUpdate', $user->id], 'method' => 'put']) !!}
         {{ Form::text('name') }}
         {{ Form::text('email') }}
         {{ Form::password('password', ['class' => 'password']) }}
